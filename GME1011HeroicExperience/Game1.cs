16c566ba6620f593950104d391a869823dc185ec
Aaron Langille <aaron.langille@gmail.com>
2024-03-13 19:30:34 -0400

    Add project files.

---
+++ GME1011HeroicExperience/Game1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GME1011HeroicExperience
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        //Our attributes
+        private SpriteFont _gamefont;
+        private Hero _myHero;
+        private Canonball _canonball;
+        private Texture2D _heroSprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _gamefont = Content.Load<SpriteFont>("GameFont");
+
+            _heroSprite = Content.Load<Texture2D>("hero-small");
+            _myHero = new Hero(5, "Aaron", _heroSprite, _gamefont);
+
+            _canonball = new Canonball(Content.Load<Texture2D>("Canonball_Sprite"));
+
+            // TODO: use this.Content to load your game content here
+        }
+
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            
+            _myHero.Update(gameTime);
+            if (_myHero.CollidesWithCanonball(_canonball))
+            {
+                _myHero.TakeDamage(1);
+                _canonball = null;
+            }
+
+
+
+            if(_canonball != null)
+                _canonball.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _myHero.Draw(_spriteBatch);
+
+            if (_canonball != null)
+                _canonball.Draw(_spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GME1011HeroicExperience/Hero.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GME1011HeroicExperience
+{
+    internal class Hero
+    {
+        private int _health;
+        private string _name;
+        private Texture2D _currentSprite;
+        private SpriteFont _heroFont;
+        private float _heroX, _heroY;
+
+        public Hero(int health, string name, Texture2D currentSprite, SpriteFont heroFont)
+        {
+            _health = health;
+            _name = name;
+            _currentSprite = currentSprite;
+            _heroX = 100;
+            _heroY = 10;
+            _heroFont = heroFont;
+         }
+
+        public int GetHealth() {  return _health; }
+        public string GetName() { return _name; }   
+
+        public int DealDamage() { Random _rng = new Random(); return _rng.Next(3, 10);  }
+        public void TakeDamage(int damage) { _health -= damage; }
+
+        public void Update(GameTime gameTime) 
+        {
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                _heroY--;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                _heroY++;
+
+           
+        }
+
+        public bool CollidesWithCanonball(Canonball danger)
+        {
+            if(danger == null) {  return false; }
+            Rectangle myBounds = GetBounds();
+            Rectangle dangerBounds = danger.GetBounds();
+            if (myBounds.Intersects(dangerBounds))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Rectangle GetBounds()
+        {
+            return new Rectangle((int)_heroX, (int)_heroY, _currentSprite.Width, _currentSprite.Height);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(_heroFont, _health + "", new Vector2(_currentSprite.Width / 2, _heroY - 60), Color.White);
+            spriteBatch.Draw(_currentSprite, new Vector2(_heroX, _heroY), Color.White);
+            spriteBatch.End();
+        }
+
+        public override string ToString() { return "Hero[" + _name + "," + _health + "]";}
+
+    }
+}
